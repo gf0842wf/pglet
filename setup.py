d7954fe51ba11bd785e3c94bd6069c563ae90b78
gf0842wf <gf0842wf@gmail.com>
2014-12-22 16:25:17 +0800

    setup
    Signed-off-by: gf0842wf <gf0842wf@gmail.com>

--- setup.py
+++ setup.py
@@ -1,29 +1,31 @@
 # -*- coding: utf-8 -*-
-from setuptools import setup, find_packages
+#!/usr/bin/env python
 
+import sys
+import os
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
-setup(
-    name='pglet',
-    version='0.0.1',
-    description='gevent multi process & multi greenlet',
-    long_description=open('README.md').read(),
-    author='fk',
-    author_email='gf0842wf@gmail.com',
-    url='https://github.com/gf0842wf/pglet',
-    packages=find_packages(),
-    license=open('LICENSE').read(),
-    include_package_data=True,
-    keywords=['python', 'gevent', 'multiprocessing'],
-    classifiers=(
-        'Development Status :: 4 - Beta',
+import pglet
+
+setup(name='pglet',
+      version='0.0.1',
+      description='Fast and simple WSGI-framework for small web-applications.',
+      long_description='gevent multi process & multi greenlet',
+      author='fk',
+      author_email='marc@gsites.de',
+      url='https://github.com/gf0842wf/pglet',
+      py_modules=['pglet'],
+      scripts=['pglet.py'],
+      license='MIT',
+      platforms = '*nix',
+      classifiers=['Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
-        'Natural Language :: English',
-        'License :: OSI Approved :: MIT',
-        'Programming Language :: Python',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
-    ),
-    install_requires=[
-        'gevent',
-        'gipc',
-    ]
-)
+        ],
+     )
