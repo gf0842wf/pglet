6ab35dd91e71e962e57465e848fc0f247c614f95
gf0842wf <gf0842wf@gmail.com>
2014-12-22 16:14:21 +0800

    setup
    Signed-off-by: gf0842wf <gf0842wf@gmail.com>

--- setup.py
+++ setup.py
@@ -1,45 +1,29 @@
 # -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
 
-from distutils.core import setup
-import os.path
-import sys
-
-import pglet
-
-
-classifiers = [
-    "Programming Language :: Python",
-    "Programming Language :: Python :: 2",
-    "Intended Audience :: Developers",
-    "License :: OSI Approved :: MIT",
-    "Operating System :: *nix",
-    "Topic :: Software Development :: Libraries :: Python Modules",
-    ]
-
-
-def read(fname):
-    fname = os.path.join(os.path.dirname(__file__), fname)
-    if sys.version > '3.0':
-        content = open(fname, encoding='utf-8').read()
-    else:
-        content = open(fname).read().decode('utf-8')
-    return content.strip()
-
-def read_files(*fnames):
-    return '\r\n\r\n\r\n'.join(map(read, fnames))
 
 setup(
-    name = 'pglet',
+    name='pglet',
     version='0.0.1',
-    packages = [
-        #'pglet',
-        ],
-    description = 'gevent multi process & multi greenlet',
-    long_description = read_files('README.md', ),
-    license = 'MIT',
-    author = 'gf0842wf',
-    author_email = 'gf0842wf@gmail.com',
-    url = 'https://github.com/gf0842wf/pglet',
-    keywords = ['python', 'gevent', 'multiprocessing'],
-    classifiers = classifiers, 
-    )
+    description='gevent multi process & multi greenlet',
+    long_description=open('README.md').read(),
+    author='fk',
+    author_email='gf0842wf@gmail.com',
+    url='https://github.com/gf0842wf/pglet',
+    packages=find_packages(),
+    license=open('LICENSE').read(),
+    include_package_data=True,
+    keywords=['python', 'gevent', 'multiprocessing'],
+    classifiers=(
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+    ),
+    install_requires=[
+        'gevent',
+        'gipc',
+    ]
+)
