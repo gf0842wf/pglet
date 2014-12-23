cf7ae9b80f354aa0444dcd97ed92a7865a5c5361
gf0842wf <gf0842wf@gmail.com>
2014-12-23 17:09:53 +0800

    unit test
    Signed-off-by: gf0842wf <gf0842wf@gmail.com>

--- tests/test_pglet.py
+++ tests/test_pglet.py
@@ -3,23 +3,28 @@
 import unittest
 
 from pglet import PPool
+import gevent
 
 import logging
 logger = logging.getLogger(__name__)
 
 
-class PgletTestCase(unittest.TestCase):
+def task_func(x):
+    return x
     
+    
+class PgletTestCase(unittest.TestCase):
+        
     def setUp(self):
         logging.basicConfig(level=logging.DEBUG, format='[%(asctime)-15s %(levelname)s:%(module)s] %(message)s')
         ppool = PPool(2)
         self._ppool = ppool
         self._ppool.init()
-        self._task_func = lambda x : x
+        
+        gevent.spawn(gevent.sleep, 5).join()
 
     def test_spawn(self):
         ppool = self._ppool
-        task_func = self._task_func
         
         future = ppool.spawn(task_func, 1)
         self.assertIsNotNone(future)
@@ -27,10 +32,13 @@ class PgletTestCase(unittest.TestCase):
         
     def test_spawn_sub(self):
         ppool = self._ppool
-        task_func = self._task_func
         
-        result = ppool.spawn(task_func, 1)
+        result = ppool.spawn_sub(task_func, 1)
         self.assertIsNone(result)
+        
+    def tearDown(self):
+        ppool = self._ppool
+        ppool.close()
         
 if __name__ == "__main__":
     unittest.main()        
